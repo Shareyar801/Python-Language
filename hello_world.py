682136b2d09898eea7b1e9bc2bdda5d5d864c1bd
shareyar <shareyar8@gmail.com>
2022-09-25 00:31:59 +0500

    commit now

--- hello_world.py
+++ hello_world.py
@@ -5,13 +5,28 @@ int_var2 = 2 ** 3
 weight = 88.914
 gender = 'M'
 name = "Shammmmryar " \
-       "farooqui"  # multi line string input but however it will provide output in single line.
-married: bool = False
+       "farooqui"  
+# multi line string input but however it will provide output in single line.
 
-print(type(married))
+married: bool = False
+about = ["born in Islamabad", "lives in karachi", "previous residence: hyderabad", 2001, 5.8]
+about.append('share')
+about2: list = about + ["doing bsit"]
+list_mein_list = [['a', '1'], ['b', '2']]
+
+print(married)
+print(int_var2)
+print(about)
+print(about2)
+print(list_mein_list)
+
+#Negative List Indices: there are 3 intences in this case.
+print(about[-2])
+print()
+
 
 if not age < 18 or age == 18:
-    print("the are " + str(age) + " so you can vote.")
+    print("Your age is " + str(age) + " so you can vote.")
 elif age < 18:
     print("You are under legal age of 18 to vote.")
 
