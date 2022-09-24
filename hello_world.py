be9226ebd82a93cddd90bb75868108c328c70385
shareyar <shareyar8@gmail.com>
2022-09-25 01:08:40 +0500

    work till list is done. next is loop n functions

--- hello_world.py
+++ hello_world.py
@@ -11,8 +11,15 @@ name = "Shammmmryar " \
 married: bool = False
 about = ["born in Islamabad", "lives in karachi", "previous residence: hyderabad", 2001, 5.8]
 about.append('share')
+about.insert(3, 'share')
 about2: list = about + ["doing bsit"]
+about2.remove(2001)
+print(about.pop)
+print(about.count('why'))
+print(len(about))
 list_mein_list = [['a', '1'], ['b', '2']]
+list_mein_list.append(['c', '3'])
+list_mein_list[1][0] = 'B'
 
 print(married)
 print(int_var2)
@@ -21,8 +28,9 @@ print(about2)
 print(list_mein_list)
 
 #Negative List Indices: there are 3 intences in this case.
-print(about[-2])
-print()
+print(about[-2])  #No 1:a specfic value.
+print(about[-3:]) #No 2:last 3 values
+print(about[:-3]) #No 3:first 3 values.
 
 
 if not age < 18 or age == 18:
