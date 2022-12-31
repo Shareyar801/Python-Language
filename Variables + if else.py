d9a920a2140a8e765550a571ef63a68d5714057a
Shareyar Farooqui <87834019+Shareyar801@users.noreply.github.com>
2023-01-01 00:19:56 +0500

    Update Variables + if else.py

--- Variables + if else.py
+++ Variables + if else.py
@@ -10,7 +10,7 @@ def basics_variables():
         "farooqui"  
     married: bool = False
 
-    print("PRINTING DATA OF VARIABLES:\n")
+    print("PRINTING DATA OF VARIABLES:")
     print(married)
     print(int_var2)
 
@@ -27,40 +27,4 @@ def basics_variables():
         print("You are a female")   
 
 
-def list_working():
-    about = ["born in Islamabad", "lives in karachi", "previous residence: hyderabad", 2001, 5.8]
-    about.append('share')
-    about.insert(3, 'share')
-
-    about.pop
-    print("\nLIST 01: ABOUT:- ")
-    print(about.count('why'))
-    print(len(about))
-    print(about)
-
-    print("\nLIST 02: ABOUT2:- ")
-    about2: list = about + ["doing bsit"]
-    about2.remove(2001)
-    print(about2)
-
-    #Negative List Indices: there are 3 intences in this case.
-    print(about[-2])  #No 1:a specfic value.
-    print(about[-3:]) #No 2:last 3 values
-    print(about[:-3]) #No 3:first 4(3+0) values.
-
-    print("\nLIST 03: LIST MEIN LIST:- ")
-    list_mein_list = [['a', '1'], ['b', '2']]
-    list_mein_list.append(['c', '3'])
-    list_mein_list[1][0] = 'B'
-    print(list_mein_list)
-    list_mein_list = [['a', ['2', 2]], ['b', '2']]
-    print(list_mein_list)
-
-
-list_working()
 basics_variables()
-
-
-
-
-
